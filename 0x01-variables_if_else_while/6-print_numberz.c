0184f7050aed91b7b00bedd77006b274c18e9cb5
Alex-KR-dev <ianalex582@gmail.com>
2023-03-20 14:55:31 -0700

    Use of Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/*
+ * File: 6-print_numberz.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
